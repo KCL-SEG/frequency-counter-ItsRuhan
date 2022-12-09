a71992744a1c7d6ec69fdbc4b2d7c5c7055c15d2
Ruhan Mohammad Salam <k21056749@kcl.ac.uk>
2022-12-09 11:27:41 +0000

    Add method body

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,9 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for v in items:
+        v = str(v)
+    frequencies = dict.fromkeys(items, 0)
+    for k, v in frequencies.items():
+        frequencies[k] = items.count(v)
     return frequencies
